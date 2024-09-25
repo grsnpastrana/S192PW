ca27271f0070f2f41b1a52dc011bb9bc54ef3a7b
grsnpastrana <grsnpastrana@gmail.com>
2024-09-24 18:56:57 -0600

    avance practica 3

--- introLaravel/resources/views/welcome.blade.php
+++ introLaravel/resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js']) 
-    <title>Document</title>
+    <title>Inicio</title>
 </head>
 <body>
-    <h1> HOLA MUNDO</h1>
+    <h1> Inicio </h1>
 </body>
 </html>
