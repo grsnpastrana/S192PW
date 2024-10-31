6c0e7a78671025e6f8c04a94ea650c571fee6c6f
grsnpastrana <grsnpastrana@gmail.com>
2024-10-31 01:22:11 -0600

    creacion de rutas y yields para poner los contenidos

---
+++ segundoRepaso/resources/views/formulario.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantilla1')
+@section('titulo','Registro de libro')
+
+@section('contenido')
+
+<body>
+    <h1>hola formulario</h1>
+</body>
+
+
+@endsection  
---
+++ segundoRepaso/resources/views/inicio.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.plantilla1')
+@section('titulo','Pagina inicio')
+
+
+@section('contenido')
+
+<body>
+    <h1>hola inicio</h1>
+</body>
+
+
+@endsection  
